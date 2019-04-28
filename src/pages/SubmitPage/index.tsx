40b61ed5a7209fb404a9ca88aec2e935cd22ebd5
zhammer <zachary_hammer@alumni.brown.edu>
2019-04-27 23:57:11 -0400

    Start Submit Page

--- src/pages/SubmitPage/index.tsx
+++ src/pages/SubmitPage/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import useQueryParams from './useQueryParams';
 import Page from '../../components/Page';
+import { Redirect } from 'react-router';
+import useFetchSong from './useFetchSong';
+import Song from '../../components/Song';
 
 export default function SubmitPage() {
-  return <Page>Submit Page</Page>;
+  const queryParams = useQueryParams();
+  const songId = isString(queryParams.id) ? queryParams.id : null;
+  const [song, loading, error] = useFetchSong(songId);
+
+  if (!songId) return <Redirect to='/question' />;
+  return (
+    <Page>
+      Submit page {songId}
+      {error && <div>{error}</div>}
+      {loading && <div>loading..</div>}
+      {song && <Song song={song} />}
+    </Page>
+  );
+}
+
+function isString(val: any): val is string {
+  return typeof val === 'string';
 }
