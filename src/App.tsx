4aaef9e6684fd2f96986793defc4591995f82212
zhammer <zachary_hammer@alumni.brown.edu>
2019-04-28 17:42:22 -0400

    Add /listens route for testing

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import theme from './styles/theme';
 import { GlobalStyle } from './styles/GlobalStyle';
 import QuestionPage from './pages/QuestionPage';
 import SubmitPage from './pages/SubmitPage';
+import ListensPage from './pages/ListensPage';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
             <Switch>
               <Route exact path='/question' component={QuestionPage} />
               <Route exact path='/submit' component={SubmitPage} />
+              <Route exact path='/listens' component={ListensPage} />
               <Redirect from='/' to='/question' />
             </Switch>
           </Router>
---
+++ src/pages/ListensPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Page from '../../components/Page';
+import Title from '../../components/Title';
+
+export default function ListensPage() {
+  return (
+    <Page>
+      <Title>
+        Here are the first pieces of music people listened to today, from all over the world.
+      </Title>
+    </Page>
+  );
+}
