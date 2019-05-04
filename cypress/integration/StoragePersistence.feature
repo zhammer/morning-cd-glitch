292653f9b8c05782da2d0163a74913a1f5806332
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-04 12:17:43 -0400

    Add StoragePersistence feature

---
+++ cypress/integration/StoragePersistence.feature
@@ -0,0 +1,11 @@
+Feature: Storage Persistence
+  Some state can be persisted to the browser to speed up subsequent
+  loads of Morning CD.
+
+  Scenario: I refresh morning cd after visiting
+    Given it is day
+    When I visit "/question"
+    And I wait for the page to load
+    And I refresh the page
+    And I wait for the page to load
+    Then the browser only sent the query "SunlightWindows" 1 time
---
+++ cypress/integration/StoragePersistence/steps.js
@@ -0,0 +1,34 @@
+/* global cy Cypress*/
+/// <reference types="cypress" />
+import { Then, When } from 'cypress-cucumber-preprocessor/steps';
+
+Cypress.on('window:before:load', window => {
+  window.fetch = null;
+});
+
+beforeEach(() => {
+  cy.graphql({
+    schema: Cypress.env('GRAPHQL_SCHEMA'),
+    endpoint: '/graphql',
+    mocks: {
+      Date: () => '2018-06-12',
+      DateTime: () => '2018-06-11T09:24:32.004423'
+    }
+  });
+  cy.server();
+  cy.route('/accesstoken', 'fixture:morningcd/accessToken.json');
+});
+
+When('I wait for the page to load', () => {
+  cy.get('div[data-test=loading-cds-page]').should('not.exist');
+});
+
+When('I refresh the page', () => {
+  cy.reload();
+});
+
+Then(/the browser only sent the query "(\w+)" (\d+) times?/, (operationName, numCalls) => {
+  cy.graphqlNumCallsByOperationName().then(numCallsByOperationName => {
+    expect(numCallsByOperationName).to.have.property(operationName, numCalls);
+  });
+});
