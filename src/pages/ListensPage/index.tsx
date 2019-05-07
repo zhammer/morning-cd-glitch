936b1acca843e0a881fae468d93395e41ffa87cd
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-06 21:02:23 -0400

    Flesh out most of the Listens Page

--- src/pages/ListensPage/index.tsx
+++ src/pages/ListensPage/index.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Page from '../../components/Page';
 import Title from '../../components/Title';
+import Listen from './Listen';
+import useFetchListens from './useFetchListens';
+import List from '../../components/List';
+import { useGnomon } from '../../hooks/useSundial';
+import usePrevious from '../../hooks/usePrevious';
+import { Redirect } from 'react-router';
+import StylelessLink from '../../components/StylelessLink';
 
 export default function ListensPage() {
+  const { listens, loading } = useFetchListens();
+  const [timeOfDay] = useGnomon();
+  const previousTimeOfDay = usePrevious(timeOfDay);
+  const sunHasRisen = useMemo<boolean>(() => {
+    return timeOfDay === 'day' && previousTimeOfDay === 'beforeSunrise';
+  }, [timeOfDay, previousTimeOfDay]);
+
+  if (sunHasRisen) return <Redirect to='/question' />;
   return (
     <Page>
       <Title>
         Here are the first pieces of music people listened to today, from all over the world.
       </Title>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <List>
+          {listens.reverse().map(listen => (
+            <Listen key={listen.id} listen={listen} />
+          ))}
+        </List>
+      )}
     </Page>
   );
 }
