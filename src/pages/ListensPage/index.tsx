44315b50e34a10c94eb9f726ed30cae394809600
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-10 19:44:37 -0400

    Use a map for titleByPageState

--- src/pages/ListensPage/index.tsx
+++ src/pages/ListensPage/index.tsx
@@ -11,13 +11,16 @@ import VisibilitySensor from 'react-visibility-sensor';
 import { LoadingMore } from './ListenPage.styles';
 import Text from '../../components/Text';
 
-const TITLE_LISTENS_EXIST =
-  'Here are the first pieces of music people listened to today, from all over the world.';
-const TITLE_NO_LISTENS_EXIST =
-  'Nobody posted a listen to morning.cd today. Check back here later tonight. Morning.cd works all around the world, and it’s daytime somewhere.';
-const TITLE_LOADING = 'Loading listens...';
-
 type PageState = 'loading' | 'loaded.listensExist' | 'loaded.noListensExist' | 'loaded.error';
+
+const titleByPageState: { [key in PageState]: JSX.Element | string } = {
+  loading: 'Loading listens...',
+  'loaded.listensExist':
+    'Here are the first pieces of music people listened to today, from all over the world.',
+  'loaded.noListensExist':
+    'Nobody posted a listen to morning.cd today. Check back here later tonight. Morning.cd works all around the world, and it’s daytime somewhere.',
+  'loaded.error': <Text.Error>There was an error getting today's listens.</Text.Error>
+};
 
 export default function ListensPage() {
   const { listens, loading, fetchMore, hasMore, loadingMore, error } = useFetchListens();
@@ -36,14 +39,7 @@ export default function ListensPage() {
   if (sunHasRisen) return <Redirect to='/question' />;
   return (
     <Page>
-      <Title>
-        {pageState === 'loading' && TITLE_LOADING}
-        {pageState === 'loaded.noListensExist' && TITLE_NO_LISTENS_EXIST}
-        {pageState === 'loaded.listensExist' && TITLE_LISTENS_EXIST}
-        {pageState === 'loaded.error' && (
-          <Text.Error>There was an error getting today's listens.</Text.Error>
-        )}
-      </Title>
+      <Title>{titleByPageState[pageState]}</Title>
       {pageState === 'loaded.listensExist' && (
         <>
           <List data-test='listens'>
