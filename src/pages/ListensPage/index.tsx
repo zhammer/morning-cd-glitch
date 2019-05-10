034f28a1a619261362d100ceb92ce1b86af19109
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-10 18:37:17 -0400

    Use enumerated states for listens page

--- src/pages/ListensPage/index.tsx
+++ src/pages/ListensPage/index.tsx
@@ -14,6 +14,9 @@ const TITLE_LISTENS_EXIST =
   'Here are the first pieces of music people listened to today, from all over the world.';
 const TITLE_NO_LISTENS_EXIST =
   'Nobody posted a listen to morning.cd today. Check back here later tonight. Morning.cd works all around the world, and it’s daytime somewhere.';
+const TITLE_LOADING = 'Loading listens...';
+
+type PageState = 'loading' | 'loaded.listensExist' | 'loaded.noListensExist';
 
 export default function ListensPage() {
   const { listens, loading, fetchMore, hasMore, loadingMore } = useFetchListens();
@@ -22,30 +25,35 @@ export default function ListensPage() {
   const sunHasRisen = useMemo<boolean>(() => {
     return timeOfDay === 'day' && previousTimeOfDay === 'beforeSunrise';
   }, [timeOfDay, previousTimeOfDay]);
+  const pageState = useMemo<PageState>(() => {
+    if (loading) return 'loading';
+    if (listens.length === 0) return 'loaded.noListensExist';
+    return 'loaded.listensExist';
+  }, [listens, loading]);
 
   if (sunHasRisen) return <Redirect to='/question' />;
   return (
     <Page>
-      <Title>{listens.length === 0 ? TITLE_NO_LISTENS_EXIST : TITLE_LISTENS_EXIST}</Title>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        listens.length > 0 && (
-          <>
-            <List data-test='listens'>
-              {listens.reverse().map(listen => (
-                <Listen key={listen.id} listen={listen} />
-              ))}
-            </List>
-            {hasMore && (
-              <VisibilitySensor onChange={isVisible => isVisible && fetchMore()}>
-                <LoadingMore data-test='more-listens-loader' active={loadingMore}>
-                  Loading more...
-                </LoadingMore>
-              </VisibilitySensor>
-            )}
-          </>
-        )
+      <Title>
+        {pageState === 'loading' && TITLE_LOADING}
+        {pageState === 'loaded.noListensExist' && TITLE_NO_LISTENS_EXIST}
+        {pageState === 'loaded.listensExist' && TITLE_LISTENS_EXIST}
+      </Title>
+      {pageState === 'loaded.listensExist' && (
+        <>
+          <List data-test='listens'>
+            {listens.reverse().map(listen => (
+              <Listen key={listen.id} listen={listen} />
+            ))}
+          </List>
+          {hasMore && (
+            <VisibilitySensor onChange={isVisible => isVisible && fetchMore()}>
+              <LoadingMore data-test='more-listens-loader' active={loadingMore}>
+                Loading more...
+              </LoadingMore>
+            </VisibilitySensor>
+          )}
+        </>
       )}
     </Page>
   );
