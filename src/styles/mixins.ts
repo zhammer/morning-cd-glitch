211c54f2905c5df88581bb535a828dc1fc3c931f
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-11 13:09:25 -0400

    Remove borderImage mixin -- not being used anywhere

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -10,11 +10,6 @@ function rgb(hexString: string) {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-export const borderImage = (color: string) =>
-  css`border-image-source: url('data:image/svg+xml;utf8,<?xml version="1.0" encoding="UTF-8" ?><svg version="1.1" width="8" height="8" xmlns="http://www.w3.org/2000/svg"><path d="M3 1 h1 v1 h-1 z M4 1 h1 v1 h-1 z M2 2 h1 v1 h-1 z M5 2 h1 v1 h-1 z M1 3 h1 v1 h-1 z M6 3 h1 v1 h-1 z M1 4 h1 v1 h-1 z M6 4 h1 v1 h-1 z M2 5 h1 v1 h-1 z M5 5 h1 v1 h-1 z M3 6 h1 v1 h-1 z M4 6 h1 v1 h-1 z" fill="${rgb(
-    color
-  )}" /></svg>');`;
-
 export const compactBorderImage = css`
   border-image-source: url('data:image/svg+xml;utf8,<?xml version="1.0" encoding="UTF-8" ?><svg version="1.1" width="5" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2 1 h1 v1 h-1 z M1 2 h1 v1 h-1 z M3 2 h1 v1 h-1 z M2 3 h1 v1 h-1 z" fill="${props =>
     rgb(props.theme.base)}" /></svg>');`;
