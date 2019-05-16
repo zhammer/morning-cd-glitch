d3197396800f51d1d12aa095bd70b3f70f737ddf
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-05-15 22:19:25 -0400

    Update index.ts

--- src/components/Page/index.ts
+++ src/components/Page/index.ts
@@ -1,16 +1,9 @@
 import styled from '../../custom/styled-components';
 import { mobileBreakpoint } from '../../styles/variables';
-import { keyframes } from 'styled-components';
-
-const fadein = keyframes`
-  from { opacity: 0 }
-  to   { opacity: 1 }
-`;
 
 const Page = styled.div`
   width: 90%;
   margin: 0 auto;
-  animation: ${fadein} 0.25s linear both;
   margin-bottom: 2em;
   padding-top: 2em;
 
