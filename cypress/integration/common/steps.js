c9a617780d034720a7670137cdd98ff11841149b
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-18 19:11:56 -0400

    Move clear service worker code from common to support

--- cypress/integration/common/steps.js
+++ cypress/integration/common/steps.js
@@ -7,16 +7,6 @@ beforeEach(() => {
   cy.wrap(['Date']).as('clockOverrides');
 });
 
-beforeEach(() => {
-  if (window.navigator && navigator.serviceWorker) {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      registrations.forEach(registration => {
-        registration.unregister();
-      });
-    });
-  }
-});
-
 When('Snap! *', () => {
   cy.then(() => {
     cy.screenshot();
