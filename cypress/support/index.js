zhammer/morning-cd-glitch

commit 9b241a8cee4235d4a76521ed1813f3cc9c6be5ef

SEO, perf & accessibility enhancements (#25)
* Add site icons

* Use service worker

* Add label to question input

* Remove old icon label

* Add meta description

* Add og tags

src/index.tsx
@@ -5,7 +5,4 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();

src/pages/QuestionPage/index.tsx
@@ -30,6 +30,7 @@ export default function QuestionPage() {
       <QuestionContainer>
         <Input.Text
           data-test='question-input'
+          aria-label='question-input'
           value={questionInput}
           onChange={e => setQuestionInput(e.target.value)}
           onKeyDown={e => e.key === 'Enter' && forceConfident()}

public/index.html
@@ -2,14 +2,32 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
+
+    <meta
+      property="og:image"
+      content="https://s3.amazonaws.com/morning-cd-readme-static-content/morning-cd-glitch-image.png"
+    />
+    <meta
+      property="og:description"
+      content="What was the first piece of music you listened to this morning?"
+    />
+    <meta property="og:url" content="https://8bit.morningcd.com" />
+    <meta property="og:title" content="Morning Cd - Glitch!" />
+    <meta property="og:type" content="website" />
+
+    <meta
+      name="description"
+      content="What was the first piece of music you listened to this morning?"
+    />
     <!--
       manifest.json provides metadata used when your web app is installed on a
       user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
     -->
     <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    <link rel="shortcut icon" href="%PUBLIC_URL%/icon-192.png" />
+    <link rel="apple-touch-icon" sizes="192x192" href="%PUBLIC_URL%/icon-192.png" />
     <!--
       Notice the use of %PUBLIC_URL% in the tags above.
       It will be replaced with the URL of the `public` folder during the build.
@@ -20,10 +38,7 @@
       Learn how to configure a non-root public URL by running `npm run build`.
     -->
     <title>Morning CD - Glitch</title>
-    <link
-      href="https://fonts.googleapis.com/css?family=Press+Start+2P"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet" />
   </head>
   <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>

commit 6b5414c1875dfa42ffdc41d06b814ccb2ae1f8f3

Unregister serviceWorker in app if using cypress runner
Fixes an ongoing issue started in #25 and attempted to solve in #26

cypress/support/index.js
@@ -5,14 +5,5 @@ require('cypress-plugin-tab');
 
 Cypress.on('window:before:load', window => {
   window.fetch = null;
-});
-
-beforeEach(() => {
-  if (window.navigator && navigator.serviceWorker) {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      registrations.forEach(registration => {
-        registration.unregister();
-      });
-    });
-  }
+  window.isCypressRunner = true;
 });

src/index.tsx
@@ -5,4 +5,8 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.register();
+if ((window as any).isCypressRunner) {
+  serviceWorker.unregister();
+} else {
+  serviceWorker.register();
+}