e1dca925bea242653d1458d13183b62261097eb3
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-20 14:09:30 -0400

    Add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+if (window.origin.endsWith('morningcd.com')) {
+  Sentry.init({
+    dsn: 'https://059716a475b64c9181a008f4afb335c8@sentry.io/1463806'
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
