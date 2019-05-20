f751e6eaab4a8bbf3e726b28457b67d8a7fcb8a1
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-05-20 19:41:05 -0400

    Add icons (#35)
    * Add makeIcon function
    
    * Add social icons

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,15 @@
+import Facebook from './Facebook';
+import Github from './Github';
+import Instagram from './Instagram';
+import LinkedIn from './LinkedIn';
+import Spotify from './Spotify';
+import Twitter from './Twitter';
+
+export default {
+  Facebook,
+  Github,
+  Instagram,
+  LinkedIn,
+  Spotify,
+  Twitter
+};
