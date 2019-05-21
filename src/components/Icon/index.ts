e7d6cb4e1ab08d4db5bb48ce0f7cba2b43773340
zhammer <zachary_hammer@alumni.brown.edu>
2019-05-21 19:15:02 -0400

    Add new icons

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,13 +1,17 @@
+import Cypress from './Cypress';
 import Facebook from './Facebook';
 import Github from './Github';
+import Gmail from './Gmail';
 import Instagram from './Instagram';
 import LinkedIn from './LinkedIn';
 import Spotify from './Spotify';
 import Twitter from './Twitter';
 
 export default {
+  Cypress,
   Facebook,
   Github,
+  Gmail,
   Instagram,
   LinkedIn,
   Spotify,
